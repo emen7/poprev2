1a9bddf8936ba349eb663417357757eaa040e20c
emen7 <44346972+emen7@users.noreply.github.com>
2025-04-12 09:09:02 -0400

    Add ESLint and Prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,89 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier', // Make sure this is last to override other configs
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  rules: {
+    // Base ESLint rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off', // Handled by TypeScript
+
+    // TypeScript rules
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+
+    // React rules
+    'react/prop-types': 'off', // Not needed with TypeScript
+    'react/react-in-jsx-scope': 'off', // Not needed with Next.js
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // Import rules
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+
+    // Prettier integration
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.next/',
+    'out/',
+    'build/',
+    '*.d.ts',
+    'coverage/',
+    'backup/', // Ignore the backup directory to avoid plugin conflicts
+    'docs/archive/', // Ignore archived documentation
+    'docs/archive/oldcode/', // Ignore old code
+    '**/sandbox/**', // Ignore all sandbox directories
+  ],
+};
