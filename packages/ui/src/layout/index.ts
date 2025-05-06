e00b234afa2511dde14062b235c80f78b2f53433
emen7 <44346972+emen7@users.noreply.github.com>
2025-05-06 13:49:17 -0400

    fix: add missing exports and components for UI layout

--- packages/ui/src/layout/index.ts
+++ packages/ui/src/layout/index.ts
@@ -1,1 +1,7 @@
 export * from './three-row-header';
+export { ReaderHeader } from './ReaderHeader';
+export { default as Header } from './header';
+export { default as ContentContainer } from './ContentContainer';
+export { default as Footer } from './Footer';
+export { default as MultiPurposePanel } from './panels/MultiPurposePanel';
+export { default as SidePanel } from './panels/SidePanel';
